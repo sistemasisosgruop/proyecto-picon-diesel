b7e0f1e32ab0ca26f57f0911369adb8ab27bbeaa
Eleo Huayhua <eleo.romario@gmail.com>
2022-11-21 14:50:06 -0500

    MAN004: 0.0.10 Modulo tipo de clientes, listado y agregar

---
+++ pages/mantenimiento/administrativo/tipos-de-clientes/index.js
@@ -0,0 +1,91 @@
+import { Input } from "@material-tailwind/react";
+import { useMemo } from "react";
+import {
+	ButtonAdd,
+	ButtonCancel,
+	ButtonImportData,
+	ButtonSave,
+} from "../../../../app/components/elements/Buttons";
+import { Title } from "../../../../app/components/elements/Title";
+import {
+	Modal,
+	ModalConfirmDelete,
+} from "../../../../app/components/modules/Modal";
+import TableComplete from "../../../../app/components/modules/TableComplete";
+import TemplateAdministrativo from "../../../../app/components/templates/TemplateAdministrativo";
+import { useModal } from "../../../../app/hooks/useModal";
+import { tiposDeClientes } from "../../../../data/tipos-de-clientes";
+
+export default function TiposClientes() {
+	const {
+		isOpenModal,
+		isOpenModalDelete,
+		isEdit,
+		setIsOpenModalDelete,
+		closeModal,
+		openModal,
+	} = useModal();
+
+	const saveData = () => {
+		closeModal();
+	};
+
+	const columns = useMemo(
+		() => [
+			{ Header: "#", accessor: "id" },
+			{ Header: "Codigo", accessor: "codigo" },
+			{ Header: "Tipo de cliente", accessor: "nombre" },
+			{ Header: "Estado", accessor: "estado" },
+		],
+		[]
+	);
+
+	const data = useMemo(() => tiposDeClientes, []);
+
+	return (
+		<>
+			<TemplateAdministrativo>
+				<Title text={"Lista Tipos de clientes"}>
+					<div className="flex gap-4">
+						<ButtonImportData />
+						<ButtonAdd
+							text={"Nuevo tipo de cliente"}
+							onClick={() => openModal(false)}
+						/>
+					</div>
+				</Title>
+				{/* Table list */}
+				<TableComplete
+					columns={columns}
+					data={data}
+					openModal={openModal}
+					setIsOpenModalDelete={setIsOpenModalDelete}
+				/>
+			</TemplateAdministrativo>
+			{/* Modal agregar */}
+			<Modal
+				title={
+					isEdit ? "Editar Tipo de cliente" : "Nuevo Tipo de cliente"
+				}
+				isOpen={isOpenModal}
+				closeModal={closeModal}
+			>
+				{/* Form */}
+				<form className="flex flex-col gap-5">
+					<Input label="Tipo de cliente" />
+					
+					<div className="w-full flex justify-end gap-5">
+						<ButtonCancel onClick={closeModal} />
+						<ButtonSave onClick={saveData} />
+					</div>
+				</form>
+			</Modal>
+			{/* Modal Eliminar */}
+			<ModalConfirmDelete
+				title={"Eliminar Tipo de cliente"}
+				isOpen={isOpenModalDelete}
+				closeModal={() => setIsOpenModalDelete(false)}
+			/>
+		</>
+	);
+}
