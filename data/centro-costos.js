a263bdd6ffee7a08b6b81cd5e98b5c1c4fd4256b
Eleo Huayhua <eleo.romario@gmail.com>
2022-11-21 14:33:36 -0500

    MAN004: 0.0.7 Modulo Centro de costos, listado y agregar

---
+++ data/centro-costos.js
@@ -0,0 +1,23 @@
+export const centroCostos = [
+  {
+    id: 1,
+    codigo: "CC001",
+    nombre: "Centro de Costos 1",
+    responsable: "Juan Perez",
+    estado: "Activo",
+  },
+  {
+    id: 2,
+    codigo: "CC002",
+    nombre: "Centro de Costos 2",
+    responsable: "Maria Perez",
+    estado: "Activo",
+  },
+  {
+    id: 3,
+    codigo: "CC003",
+    nombre: "Centro de Costos 3",
+    responsable: "Pedro Perez",
+    estado: "Activo",
+  },
+];
--- pages/mantenimiento/administrativo/centro-de-costos/index.js
+++ pages/mantenimiento/administrativo/centro-de-costos/index.js
@@ -1,4 +1,4 @@
-import { Input, Option, Select } from "@material-tailwind/react";
+import { Input } from "@material-tailwind/react";
 import { useMemo } from "react";
 import {
 	ButtonAdd,
@@ -14,9 +14,9 @@ import {
 import TableComplete from "../../../../app/components/modules/TableComplete";
 import TemplateAdministrativo from "../../../../app/components/templates/TemplateAdministrativo";
 import { useModal } from "../../../../app/hooks/useModal";
-import { clientes } from "../../../../data/clientes";
+import { centroCostos } from "../../../../data/centro-costos";
 
-export default function Clientes() {
+export default function CentroCostos() {
 	const {
 		isOpenModal,
 		isOpenModalDelete,
@@ -35,25 +35,22 @@ export default function Clientes() {
 			{ Header: "#", accessor: "id" },
 			{ Header: "Codigo", accessor: "codigo" },
 			{ Header: "Nombre", accessor: "nombre" },
-			{ Header: "Tipo Documento", accessor: "tipoDocumento" },
-			{ Header: "N° de documento", accessor: "numeroDocumento" },
-			{ Header: "Teléfono", accessor: "telefono" },
-			{ Header: "Correo", accessor: "correo" },
+			{ Header: "Responsable", accessor: "responsable" },
 			{ Header: "Estado", accessor: "estado" },
 		],
 		[]
 	);
 
-	const data = useMemo(() => clientes, []);
+	const data = useMemo(() => centroCostos, []);
 
 	return (
 		<>
 			<TemplateAdministrativo>
-				<Title text={"Lista Clientes"}>
+				<Title text={"Lista Centro de Costos"}>
 					<div className="flex gap-4">
 						<ButtonImportData />
 						<ButtonAdd
-							text={"Nuevo cliente"}
+							text={"Nuevo centro de costos"}
 							onClick={() => openModal(false)}
 						/>
 					</div>
@@ -68,24 +65,16 @@ export default function Clientes() {
 			</TemplateAdministrativo>
 			{/* Modal agregar */}
 			<Modal
-				title={isEdit ? "Editar Cliente" : "Nuevo Cliente"}
+				title={isEdit ? "Editar Centro de costos" : "Nuevo Centro de costos"}
 				isOpen={isOpenModal}
 				closeModal={closeModal}
 			>
 				{/* Form */}
 				<form className="flex flex-col gap-5">
-					<Input label="Nombre o Razon Social" />
 					<div className="flex gap-5">
-						<Select label="Tipo de documento">
-              <Option value="dni">DNI</Option>
-              <Option value="ruc">RUC</Option>
-            </Select>
-						<Input label="N° de documento" type="number"/>
-					</div>
-					<div className="flex gap-5">
-						<Input label="Correo" type="email" />
-						<Input label="Teléfono" />
-					</div>
+					  <Input label="Nombre" />
+						<Input label="Responsable"/>
+					</div>					
 					<div className="w-full flex justify-end gap-5">
 						<ButtonCancel onClick={closeModal} />
 						<ButtonSave onClick={saveData} />
@@ -94,7 +83,7 @@ export default function Clientes() {
 			</Modal>
 			{/* Modal Eliminar */}
 			<ModalConfirmDelete
-				title={"Eliminar Cliente"}
+				title={"Eliminar Centro de Costos"}
 				isOpen={isOpenModalDelete}
 				closeModal={() => setIsOpenModalDelete(false)}
 			/>
