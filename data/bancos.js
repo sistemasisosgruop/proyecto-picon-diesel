d149d695dfb2b456dad906978c3e9a9d3e450443
Eleo Huayhua <eleo.romario@gmail.com>
2022-11-21 14:37:29 -0500

    MAN004: 0.0.8 Modulo Bancos, listado y agregado

---
+++ data/bancos.js
@@ -0,0 +1,17 @@
+export const bancos = [
+  {
+    id: 1,
+    codigo: "BAN001",
+    nombre: "Banco 1",
+  },
+  {
+    id: 2,
+    codigo: "BAN002",
+    nombre: "Banco 2",
+  },
+  {
+    id: 3,
+    codigo: "BAN003",
+    nombre: "Banco 3",
+  },
+];
---
+++ pages/mantenimiento/administrativo/bancos/index.js
@@ -0,0 +1,91 @@
+import { Input } from "@material-tailwind/react";
+import { useMemo } from "react";
+import {
+	ButtonAdd,
+	ButtonCancel,
+	ButtonImportData,
+	ButtonSave,
+} from "../../../../app/components/elements/Buttons";
+import { Title } from "../../../../app/components/elements/Title";
+import {
+	Modal,
+	ModalConfirmDelete,
+} from "../../../../app/components/modules/Modal";
+import TableComplete from "../../../../app/components/modules/TableComplete";
+import TemplateAdministrativo from "../../../../app/components/templates/TemplateAdministrativo";
+import { useModal } from "../../../../app/hooks/useModal";
+import { bancos } from "../../../../data/bancos";
+
+export default function Bancos() {
+	const {
+		isOpenModal,
+		isOpenModalDelete,
+		isEdit,
+		setIsOpenModalDelete,
+		closeModal,
+		openModal,
+	} = useModal();
+
+	const saveData = () => {
+		closeModal();
+	};
+
+	const columns = useMemo(
+		() => [
+			{ Header: "#", accessor: "id" },
+			{ Header: "Codigo", accessor: "codigo" },
+			{ Header: "Nombre", accessor: "nombre" },
+		],
+		[]
+	);
+
+	const data = useMemo(() => bancos, []);
+
+	return (
+		<>
+			<TemplateAdministrativo>
+				<Title text={"Lista Bancos"}>
+					<div className="flex gap-4">
+						<ButtonImportData />
+						<ButtonAdd
+							text={"Nuevo banco"}
+							onClick={() => openModal(false)}
+						/>
+					</div>
+				</Title>
+				{/* Table list */}
+				<TableComplete
+					columns={columns}
+					data={data}
+					openModal={openModal}
+					setIsOpenModalDelete={setIsOpenModalDelete}
+				/>
+			</TemplateAdministrativo>
+			{/* Modal agregar */}
+			<Modal
+				title={
+					isEdit
+						? "Editar Banco"
+						: "Nuevo Banco"
+				}
+				isOpen={isOpenModal}
+				closeModal={closeModal}
+			>
+				{/* Form */}
+				<form className="flex flex-col gap-5">
+						<Input label="Nombre" />
+					<div className="w-full flex justify-end gap-5">
+						<ButtonCancel onClick={closeModal} />
+						<ButtonSave onClick={saveData} />
+					</div>
+				</form>
+			</Modal>
+			{/* Modal Eliminar */}
+			<ModalConfirmDelete
+				title={"Eliminar Banco"}
+				isOpen={isOpenModalDelete}
+				closeModal={() => setIsOpenModalDelete(false)}
+			/>
+		</>
+	);
+}
