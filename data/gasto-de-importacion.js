dba8cc847c253c709923051bebfaa1552ed79bef
Eleo Huayhua <eleo.romario@gmail.com>
2022-11-21 15:29:48 -0500

    MAN005: 0.0.4 Modulo gasto de importacion, listado y agregar

---
+++ data/gasto-de-importacion.js
@@ -0,0 +1,32 @@
+export const gastosDeImportacion = [
+	{
+		id: 1,
+		codigo: "GAS001",
+		nombre: "Gasto de importacion 1",
+		descripcion: "Descripcion del gasto de importacion 1",
+	},
+	{
+		id: 2,
+		codigo: "GAS002",
+		nombre: "Gasto de importacion 2",
+		descripcion: "Descripcion del gasto de importacion 2",
+	},
+	{
+		id: 3,
+		codigo: "GAS003",
+		nombre: "Gasto de importacion 3",
+		descripcion: "Descripcion del gasto de importacion 3",
+	},
+	{
+		id: 4,
+		codigo: "GAS004",
+		nombre: "Gasto de importacion 4",
+		descripcion: "Descripcion del gasto de importacion 4",
+	},
+	{
+		id: 5,
+		codigo: "GAS005",
+		nombre: "Gasto de importacion 5",
+		descripcion: "Descripcion del gasto de importacion 5",
+	},
+];
---
+++ pages/mantenimiento/importacion/gastos-de-importacion/index.js
@@ -0,0 +1,89 @@
+import { Input, Textarea } from "@material-tailwind/react";
+import { useMemo } from "react";
+import {
+	ButtonAdd,
+	ButtonCancel,
+	ButtonImportData,
+	ButtonSave,
+} from "../../../../app/components/elements/Buttons";
+import { Title } from "../../../../app/components/elements/Title";
+import {
+	Modal,
+	ModalConfirmDelete,
+} from "../../../../app/components/modules/Modal";
+import TableComplete from "../../../../app/components/modules/TableComplete";
+import TemplateImportacion from "../../../../app/components/templates/TemplateImportacion";
+import { useModal } from "../../../../app/hooks/useModal";
+import { gastosDeImportacion } from "../../../../data/gasto-de-importacion";
+
+export default function GastosImportacion() {
+	const {
+		isOpenModal,
+		isOpenModalDelete,
+		isEdit,
+		setIsOpenModalDelete,
+		closeModal,
+		openModal,
+	} = useModal();
+
+	const saveData = () => {
+		closeModal();
+	};
+
+	const columns = useMemo(
+		() => [
+			{ Header: "#", accessor: "id" },
+			{ Header: "Codigo", accessor: "codigo" },
+			{ Header: "Nombre", accessor: "nombre" },
+			{ Header: "Descripción", accessor: "descripcion" },
+		],
+		[]
+	);
+
+	const data = useMemo(() => gastosDeImportacion, []);
+
+	return (
+		<>
+			<TemplateImportacion>
+				<Title text={"Lista Gastos de importación"}>
+					<div className="flex gap-4">
+						<ButtonImportData />
+						<ButtonAdd
+							text={"Nuevo gasto"}
+							onClick={() => openModal(false)}
+						/>
+					</div>
+				</Title>
+				{/* Table list */}
+				<TableComplete
+					columns={columns}
+					data={data}
+					openModal={openModal}
+					setIsOpenModalDelete={setIsOpenModalDelete}
+				/>
+			</TemplateImportacion>
+			{/* Modal agregar */}
+			<Modal
+				title={isEdit ? "Editar gasto de importación" : "Nuevo gasto de importación"}
+				isOpen={isOpenModal}
+				closeModal={closeModal}
+			>
+				{/* Form */}
+				<form className="flex flex-col gap-5">
+					<Input label="Nombre" />
+					<Textarea label="Descripción" />
+					<div className="w-full flex justify-end gap-5">
+						<ButtonCancel onClick={closeModal} />
+						<ButtonSave onClick={saveData} />
+					</div>
+				</form>
+			</Modal>
+			{/* Modal Eliminar */}
+			<ModalConfirmDelete
+				title={"Eliminar Gasto de importación"}
+				isOpen={isOpenModalDelete}
+				closeModal={() => setIsOpenModalDelete(false)}
+			/>
+		</>
+	);
+}
