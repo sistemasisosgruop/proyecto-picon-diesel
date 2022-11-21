e42b00a628a7f2a6303cf923e8461629f4da3648
Eleo Huayhua <eleo.romario@gmail.com>
2022-11-21 15:39:26 -0500

    MAN005: 0.0.5 Modulo factor de internamiento, listado y agregar

---
+++ data/factor-de-internamiento.js
@@ -0,0 +1,12 @@
+export const factorDeInternamiento = [
+  {
+    id: 1,
+    codigo: "FAC001",
+    valor: 0.1,
+  },
+  {
+    id: 2,
+    codigo: "FAC002",
+    valor: 0.2,
+  },
+]
---
+++ pages/mantenimiento/importacion/factor-de-internamiento/index.js
@@ -0,0 +1,91 @@
+import { Input } from "@material-tailwind/react";
+import { useMemo } from "react";
+import {
+	ButtonAdd,
+	ButtonCancel,
+	ButtonImportData,
+	ButtonSave,
+} from "../../../../app/components/elements/Buttons";
+import { Title } from "../../../../app/components/elements/Title";
+import {
+	Modal,
+	ModalConfirmDelete,
+} from "../../../../app/components/modules/Modal";
+import TableComplete from "../../../../app/components/modules/TableComplete";
+import TemplateImportacion from "../../../../app/components/templates/TemplateImportacion";
+import { useModal } from "../../../../app/hooks/useModal";
+import { factorDeInternamiento } from "../../../../data/factor-de-internamiento";
+
+export default function FactorInternamiento() {
+	const {
+		isOpenModal,
+		isOpenModalDelete,
+		isEdit,
+		setIsOpenModalDelete,
+		closeModal,
+		openModal,
+	} = useModal();
+
+	const saveData = () => {
+		closeModal();
+	};
+
+	const columns = useMemo(
+		() => [
+			{ Header: "#", accessor: "id" },
+			{ Header: "Codigo", accessor: "codigo" },
+			{ Header: "Valor", accessor: "valor" },
+		],
+		[]
+	);
+
+	const data = useMemo(() => factorDeInternamiento, []);
+
+	return (
+		<>
+			<TemplateImportacion>
+				<Title text={"Lista Factor de internamiento"}>
+					<div className="flex gap-4">
+						<ButtonImportData />
+						<ButtonAdd
+							text={"Nuevo factor"}
+							onClick={() => openModal(false)}
+						/>
+					</div>
+				</Title>
+				{/* Table list */}
+				<TableComplete
+					columns={columns}
+					data={data}
+					openModal={openModal}
+					setIsOpenModalDelete={setIsOpenModalDelete}
+				/>
+			</TemplateImportacion>
+			{/* Modal agregar */}
+			<Modal
+				title={
+					isEdit
+						? "Editar factor de internamiento"
+						: "Nuevo factor de internamiento"
+				}
+				isOpen={isOpenModal}
+				closeModal={closeModal}
+			>
+				{/* Form */}
+				<form className="flex flex-col gap-5">
+					<Input label="Valor" type="number"/>
+					<div className="w-full flex justify-end gap-5">
+						<ButtonCancel onClick={closeModal} />
+						<ButtonSave onClick={saveData} />
+					</div>
+				</form>
+			</Modal>
+			{/* Modal Eliminar */}
+			<ModalConfirmDelete
+				title={"Eliminar factor de internamiento"}
+				isOpen={isOpenModalDelete}
+				closeModal={() => setIsOpenModalDelete(false)}
+			/>
+		</>
+	);
+}
