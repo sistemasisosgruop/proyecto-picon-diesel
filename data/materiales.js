834d99318c8488f40f43d50e7fd7fd7f64989a1a
Eleo Huayhua <eleo.romario@gmail.com>
2022-11-22 17:20:38 -0500

    MAN008: 0.0.3 Modulo materiales , listado y agregar

---
+++ data/materiales.js
@@ -0,0 +1,26 @@
+export const materiales = [
+	{
+		id: 1,
+		codigo: "MAT01",
+		nombre: "Material 1",
+		precio: 100,
+	},
+	{
+		id: 2,
+		codigo: "MAT02",
+		nombre: "Material 2",
+		precio: 200,
+	},
+	{
+		id: 3,
+		codigo: "MAT03",
+		nombre: "Material 3",
+		precio: 300,
+	},
+	{
+		id: 4,
+		codigo: "MAT04",
+		nombre: "Material 4",
+		precio: 400,
+	},
+];
