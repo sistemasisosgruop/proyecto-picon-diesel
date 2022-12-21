aaa7b574c4c9f05b05f535dbfc8aaad81846f7c0
victor benavente <templario0141@gmail.com>
2022-12-20 19:24:53 -0500

    MAN: Mantenimiento, refactor empresas handler

--- pages/api/mantenimiento/empresas/[empresaId].js
+++ pages/api/mantenimiento/empresas/[empresaId].js
@@ -1,20 +1,21 @@
+import { EmpresasService } from "../../../../backend/services/mantenimiento/empresas.service";
 
 export default async function handler(req, res) {
-    try {
-        const empresaId = Number(req.query.empresaId);
-        if (req.method === "PUT") {
-        const empresa = await EmpresasService.updateCompany(empresaId, req.body);
-        return res.status(200).json(empresa);
-        }
-    
-        if (req.method === "DELETE") {
-        const empresa = await EmpresasService.deleteCompany(empresaId);
-        return res.status(200).json(empresa);
-        }
-    } catch (error) {
-        if (error.code === "P2025") {
-        return res.status(404).json({ error: error.meta.cause });
-        }
-        return res.status(400).json({ error: error.message });
+  try {
+    const empresaId = Number(req.query.empresaId);
+    if (req.method === "PUT") {
+      const empresa = await EmpresasService.updateEmpresa(empresaId, req.body);
+      return res.status(200).json(empresa);
     }
+
+    if (req.method === "DELETE") {
+      const empresa = await EmpresasService.deleteEmpresa(empresaId);
+      return res.status(200).json(empresa);
+    }
+  } catch (error) {
+    if (error.code === "P2025") {
+      return res.status(404).json({ error: error.meta.cause });
+    }
+    return res.status(400).json({ error: error.message });
+  }
 }
