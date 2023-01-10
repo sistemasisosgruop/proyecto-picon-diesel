3ad4b27da67f8cc65a76c895ba6472594ce7fc16
victor benavente <templario0141@gmail.com>
2023-01-09 19:45:10 -0500

    MAN: crear endpoints para cargar csv, backend

---
+++ pages/api/mantenimiento/presupuesto/familias/upload/index.js
@@ -0,0 +1,51 @@
+import { Formidable } from "formidable";
+import csv from "csvtojson";
+import prisma from "../../../../../../backend/prisma";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const readFile = (req) => {
+  const form = new Formidable();
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) reject(err);
+      resolve({ fields, files });
+    });
+  });
+};
+
+export default async function handler(req, res) {
+  try {
+    const empresaId = Number(req.query.empresaId);
+    if (req.method === "POST") {
+      const data = await readFile(req);
+      const csvData = await csv().fromFile(data.files.file.filepath);
+      let count = 0;
+
+      await Promise.all(
+        csvData.map(async ({ ...info }) => {
+          await prisma.familiaPresupuesto
+            .create({
+              data: {
+                empresaId,
+                ...info,
+              },
+            })
+            .then(() => count++)
+            .catch(() => {
+              console.error(`Fallo al crear ${JSON.stringify(info)}`);
+            });
+        })
+      );
+      console.log(`Sincronizacion de datos terminada, registros creados: ${count}`);
+
+      res.status(200).json({ message: "data sincronizada exitosamente" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
