9cc1ef0f8478af8bd54836e57da86c7231c2197a
CristianHuilcapaz <cristian.hp.tec@hotmail.com>
2024-10-11 16:45:15 -0500

    SET PERMISOS MIDDLEWARE

--- pages/api/mantenimiento/maestro-de-codigos/familias/index.js
+++ pages/api/mantenimiento/maestro-de-codigos/familias/index.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
   try {
     const user = await AuthService.ValidateAccessToken(req, res);
     user && AuthService.AdministradorFeatures(user.roles);
+    await AuthService.validarPermiso(user.username, req.method, req.url);
 
     if (req.method === "POST") {
       const result = await FamiliaService.createFamilia(req.body);
@@ -17,7 +18,6 @@ export default async function handler(req, res) {
       return res.status(200).json({ data: result });
     }
   } catch (error) {
-
     return res
       .status(400)
       .json({ error: error.message, code: error?.code, fields: error?.meta?.target ?? [] });
